f8d935899872cd7caee831a7e482dd82aa54eaaf
Lyaman Aslanova <aslanovalyaman515@gmail.com>
2024-09-14 00:39:24 +0400

    Добавьте файлы проекта.

---
+++ NP_03._TCP_Task_Manger_Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using NP_03._TCP_Task_Manger_Client;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+var client = new TcpClient();
+client.Connect(ip, port);
+
+var stream = client.GetStream();
+var br = new BinaryReader(stream);
+var bw = new BinaryWriter(stream);
+Command command = null;
+string responce = null;
+string str = null;
+
+while (true)
+{
+    Console.WriteLine("Write command or HELP");
+    str = Console.ReadLine().ToUpper();
+    if (str == "HELP")
+    {
+        Console.WriteLine();
+        Console.WriteLine("Command list:");
+        Console.WriteLine(Command.ProccessList);
+        Console.WriteLine($"{Command.Run} <process_name>");
+        Console.WriteLine($"{Command.Kill} <process_name>");
+        Console.WriteLine("HELP");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    var input = str.Split(' ');
+
+    switch (input[0])
+    {
+        case Command.ProccessList:
+            command = new Command { Text = input[0] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            var processList = JsonSerializer.Deserialize<string[]>(responce);
+            foreach (var item in processList)
+            {
+                Console.WriteLine($"        {item}");
+            }
+            break;
+        case Command.Run:
+            if (input.Length > 1)
+            {
+                command = new Command { Text = input[0], Param = input[1] };
+                bw.Write(JsonSerializer.Serialize(command));
+                Console.WriteLine($"Sent command to run: {input[1]}");
+            }
+            else
+            {
+                Console.WriteLine("Please provide the name of the process to run.");
+            }
+            break;
+        case Command.Kill:
+            if (input.Length > 1)
+            {
+                command = new Command { Text = input[0], Param = input[1] };
+                bw.Write(JsonSerializer.Serialize(command));
+                Console.WriteLine($"Sent command to kill: {input[1]}");
+            }
+            else
+            {
+                Console.WriteLine("Please provide the name of the process to kill.");
+            }
+
+            break;
+    }
+    Console.WriteLine("Press any key to continue");
+    Console.ReadLine();
+    Console.Clear();
+
+}
---
+++ NP_03._TCP_Task_Manger_Listener/Program.cs
@@ -0,0 +1,69 @@
+﻿using NP_03._TCP_Task_Manger_Listener;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+var listener = new TcpListener(ip, port);
+
+listener.Start();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var br = new BinaryReader(stream);
+    var bw = new BinaryWriter(stream);
+    while (true)
+    {
+        var input = br.ReadString();
+        var command = JsonSerializer.Deserialize<Command>(input);
+        Console.WriteLine(command.Text);
+        Console.WriteLine(command.Param);
+        switch (command.Text)
+        {
+            case Command.ProccessList:
+                var processes = Process.GetProcesses();
+                var processesNames = JsonSerializer
+                    .Serialize(processes.Select(p => p.ProcessName));
+                bw.Write(processesNames);
+                break;
+            case Command.Run:
+                try
+                {
+                    Process.Start(command.Param);
+                    Console.WriteLine($"Started process: {command.Param}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error starting process: {ex.Message}");
+                }
+                break;
+            case Command.Kill:
+                try
+                {
+                    var processToKill = Process.GetProcessesByName(command.Param).FirstOrDefault();
+                    if (processToKill != null)
+                    {
+                        processToKill.Kill();
+                        Console.WriteLine($"Killed process: {command.Param}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Process not found: {command.Param}. Make sure the process name is correct.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error killing process: {ex.Message}. Ensure you have the necessary permissions.");
+                }
+
+                break;
+            default:
+                Console.WriteLine("Unknown command");
+                break;
+        }
+    }
+}
